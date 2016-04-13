6ad45e9589e12dd0ad7ff56cb61908d61e62d1bf
HuguesM <hugues.martinelli@gmail.com>
2016-04-13 16:02:16 +0200

    Message ok

--- itta-chat/itta-chat/Scripts/Chat.js
+++ itta-chat/itta-chat/Scripts/Chat.js
@@ -6,7 +6,7 @@
     $('#succesLogout').hide();
 
     $('#cnxUser dl').html('');
-    
+
     var hubChat = $.connection.chatHub;
     hubChat.client.checkConnect = function (estConnecte) {
         console.log(estConnecte);
@@ -37,11 +37,24 @@
         $('#cnxUser dl').html('');
         for (var k in listusers) {
             console.log(k, listusers[k]);
-            if (listusers[k].Username != $('#login').val()){
-                $('#cnxUser dl').append("<dd><input class='userlist' name='user' type='radio' value='" + listusers[k].Connectioid + "' />" + listusers[k].Username + "</dd>");
+            if (listusers[k].Username != $('#login').val()) {
+                console.log("La fenetre existe : "+ $('#' + listusers[k].Username + ' ul').length)
+                if ($('#' + listusers[k].Username + ' ul').length == 0) {
+                    generateChatWindows(listusers[k].Username, listusers[k].Username);
+                }
+                $('#cnxUser dl').append("<dd><input class='userlist' name='user' type='radio' value='" + listusers[k].Username + "' />" + listusers[k].Username + "</dd>");
             }
         }
     };
+    hubChat.client.recepMessage = function (message) {
+        console.log("From : " + message.userfrom.Username);
+        console.log("Message : " + message.Message);
+        console.log("Time : " + message.Datetime_message);
+        console.log("Time : " + message.userto.Username);
+
+        addChat(message.userfrom.Username, message.Message, message.Datetime_message,  message.userto.Username);
+    };
+    window.hubChat = hubChat;
     $.connection.hub.start().done(
         function () {
             alert("cnx etablie");
@@ -54,16 +67,47 @@
                 }
             });
             $('#btnLogoff').click(function () {
-                
                     hubChat.server.onLogout($('#login').val());
-                
             });
         }
     );
-    function generateChatWindows(name,id) {
-        var start = '<div class="panel panel-primary">';
-        start = start + ' <div class="panel-heading" ><span class="glyphicon glyphicon-comment"></span> Chat avec : '+name+'</div></div>';
-        var middle = '<div><div class="panel-body"><ul class="chat"></ul></div>';
-        var end = '<div class="panel-footer"><div class="input-group"><input id="btn-input' + id + '" type="text" class="form-control input-sm" placeholder="Votre texte..." /> <span class="input-group-btn"><button class="btn btn-warning btn-sm btnSend" >Envoyer</button> </span></div></div></div>';
+    function generateChatWindows(name, id) {
+
+        var start = '<div class="col-md-12" id="' + id + '"><div class="panel panel-primary">';
+
+        start = start + '<div class="panel-heading" ><span class="glyphicon glyphicon-comment"></span> Chat avec : '+name+'</div>';
+        var middle = '<div class="panel-body"><ul class="chat"></ul></div>';
+        var end = '<div class="panel-footer"><div class="input-group"><input id="btn-input' + id + '" type="text" class="form-control input-sm" placeholder="Votre texte..." /> <span class="input-group-btn"><button class="btn btn-warning btn-sm btnSend" onclick="sendMessage();">Envoyer</button> </span></div></div></div>';
+        $('#chatList').append(start + middle + end);
+
     }
+    
+    function addChat(from, text, time, to) {
+        var pos = 'right';
+        var id = from;
+        if (from == $('#login').val()) {
+            pos = 'left';
+            id = to;
+        }
+        var txt = '<li class="' + pos + ' clearfix"><div class="chat-body clearfix"><div class="header"><small class=" text-muted"><span class="glyphicon glyphicon-time"></span>' + time + '</small><strong class="pull-right primary-font">' + from + '</strong></div><p>' + text + '</p></div></li>';
+        console.log(txt);
+        console.log($('#' + id + ' ul'));
+        $('#' + id + ' ul').append(txt);
+    }
+    window.addChat = addChat;
 });
+
+function sendMessage() {
+    var to = $('input[name=user]:checked').val();
+    console.log(to);
+    if (!to) {
+        alert('Merci de choisir un destinataire');
+    } else {
+        console.log("Destinataire : " + to);
+        console.log("Texte : " + $('#btn-input' + to + '').val());
+        window.hubChat.server.sendMessage($('#login').val(), to, $('#btn-input' + to + '').val());
+        window.addChat($('#login').val(), $('#btn-input' + to + '').val(), new Date().getDate(), to);
+        console.log(window.hubChat);
+        $('#btn-input' + to + '').val('');
+    }
+}
