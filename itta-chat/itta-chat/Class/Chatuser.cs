b0b111c19d6c094fc64105e8343f48623c4cf4c4
HuguesM <hugues.martinelli@gmail.com>
2016-04-13 11:30:13 +0200

    Fix bug

---
+++ itta-chat/itta-chat/Class/Chatuser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace itta_chat {
+    public class ChatUser {
+        String username;
+        String connectioid;
+        bool status;
+
+        public String Connectioid
+        {
+            get { return connectioid; }
+            set { connectioid = value; }
+        }
+
+        public String Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+
+        public bool Status
+        {
+            get { return status; }
+            set { status = value; }
+        }
+
+
+    }
+}
---
+++ itta-chat/itta-chat/Class/Message.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace itta_chat {
+    public class Messages {
+
+        ChatUser userfrom;
+        ChatUser userto;
+        String message;
+        bool stat_message;
+        DateTime datetime_message;
+
+
+        public String Message
+        {
+            get { return message; }
+            set { message = value; }
+        }
+
+        public bool Stat_message
+        {
+            get { return stat_message; }
+            set { stat_message = value; }
+        }
+
+        public DateTime Datetime_message
+        {
+            get { return datetime_message; }
+            set { datetime_message = value; }
+        }
+
+    }
+
+}
