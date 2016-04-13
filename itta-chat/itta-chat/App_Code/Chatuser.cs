7c92e7466d676161f9a8e394cea5b8ddeaab30fc
ittastudy <ittastudy>
2016-04-13 09:07:14 +0200

    méthode

---
+++ itta-chat/itta-chat/App_Code/Chatuser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace itta_chat
+{
+    public class Chatuser
+    {
+        String username;
+        String connectioid;
+        bool status;
+    }
+}
---
+++ itta-chat/itta-chat/App_Code/Message.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace itta_chat
+{
+    public class Message 
+    {
+       // Chatuser userfrom;
+        //itta_chat. userto;
+
+        String message;
+        bool stat_message;
+        DateTime datetime_message;
+
+    }
+}
